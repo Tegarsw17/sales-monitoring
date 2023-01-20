62648ef47977bf29b40ed2580f2b35e81cd76b97
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2023-01-20 10:36:58 +0700

    view untuk login(statis)

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello dev!");
-});
+app.engine(".html", require("ejs").__express);
+
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "src/view"));
+app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
+app.use("/public", express.static(path.join(__dirname, "src/view/public")));
+
+const viewSales = require("./src/routes/view/sales.route");
+app.use("/sales", viewSales);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
