d939e1620fb67ad0fb864084718dfaac723218e5
Theo <45797555+Tegarsw17@users.noreply.github.com>
2023-02-06 16:08:49 +0700

    pull back view

--- frontend/routes/view/sales.route.js
+++ frontend/routes/view/sales.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("", function (req, res) {
-  res.render("sales/main", {
+router.get("/login", function (req, res) {
+  res.render("sales/login", {
     title: "Log In",
   });
 });
@@ -10,6 +10,39 @@ router.get("", function (req, res) {
 router.get("/catalog", function (req, res) {
   res.render("sales/catalog", {
     title: "Catalog",
+  });
+});
+
+router.get("/catalog/:id", function (req, res) {
+  res.render("sales/detail", {
+    title: "Detail",
+    param: req.params.id,
+  });
+});
+
+router.get("/cart/", function (req, res) {
+  res.render("sales/cart", {
+    title: "Cart",
+    param: req.params.id,
+  });
+});
+
+router.get("/form", function (req, res) {
+  res.render("sales/form", {
+    title: "Report Form",
+  });
+});
+
+router.get("/report", function (req, res) {
+  res.render("sales/report", {
+    title: "Report",
+  });
+});
+
+router.get("/report/:id", function (req, res) {
+  res.render("sales/reportDetail", {
+    title: "Report",
+    param: req.params.id,
   });
 });
 
