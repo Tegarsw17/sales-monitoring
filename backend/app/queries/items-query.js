57fcdabce428b0d79066fbaac2b9ff52463ad9b2
yasinqurni <yalqurni@gmail.com>
2023-02-09 17:08:21 +0000

    add item description

--- backend/app/queries/items-query.js
+++ backend/app/queries/items-query.js
@@ -9,6 +9,7 @@ const createItem = async (payload, auth) => {
         user_id: auth,
         name_item: payload.name_item,
         category_id: payload.category_id,  
+        item_description: payload.description,
         price: payload.price,
         quantity: payload.quantity
     })
