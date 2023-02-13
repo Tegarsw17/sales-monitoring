ccf4b6d46eda67c7e4ee88b51b7608c376749a5d
yasinqurni <yalqurni@gmail.com>
2023-02-13 17:06:13 +0000

    update upload router

--- backend/app/routes/image_items-route.js
+++ backend/app/routes/image_items-route.js
@@ -1,8 +1,11 @@
 const {imageItemController} = require('../controllers/image_items-controller')
 const router = require('express').Router()
+const { tokenJwt } = require('../middlewares/authentication')
+const otorisasi = require('../middlewares/authorization')
 
 const imageitemcontroller = new imageItemController()
+const tokenjwt = new tokenJwt()
 
-router.post('/api/imageitem/:id',imageitemcontroller.uploadImage)
+router.post('/api/imageitem/:id',tokenjwt.verifyToken,otorisasi.authorization('admin'),imageitemcontroller.uploadImage)
 
 module.exports = router
